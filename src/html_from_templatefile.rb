96418ff28755ec75c64f6a5524fb8752ece2f8c5
Alistair <totheralistair@aol.com>
2014-11-10 09:20:03 -0600

    post-final :). starting some simple cleanup.

--- src/html_from_templatefile.rb
+++ src/html_from_templatefile.rb
@@ -1,9 +1,8 @@
-
 require 'erb'
 require 'erubis'
 
 
-def html_from_templatefile( templateFullFn, binding ) # where does this really belong?
+def html_from_templatefile( templateFullFn, binding )
   pageTemplate = Erubis::Eruby.new(File.open( templateFullFn, 'r').read)
   pageTemplate.result(binding)
 end
